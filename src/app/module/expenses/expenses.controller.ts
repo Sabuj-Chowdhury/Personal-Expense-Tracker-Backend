28315392ce575fd3e503426b486848c4afa279e3
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-08-15 04:56:22 +0600

    ADD: get all expenses

--- src/app/module/expenses/expenses.controller.ts
+++ src/app/module/expenses/expenses.controller.ts
@@ -3,6 +3,7 @@ import { Expenses } from "./expenses.model";
 
 export const expenseRoute = Router();
 
+// ******* CREATE Expense
 expenseRoute.post(
   "/",
   async (req: Request, res: Response, next: NextFunction) => {
@@ -14,6 +15,25 @@ expenseRoute.post(
       res.status(201).json({
         success: true,
         message: "Expense created successfully!",
+        data: expense,
+      });
+    } catch (error: any) {
+      next(error);
+    }
+  }
+);
+
+// ********* GET all expenses
+
+expenseRoute.get(
+  "/",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const expense = await Expenses.find();
+
+      res.status(200).json({
+        success: true,
+        message: "All expenses retrieved successfully!",
         data: expense,
       });
     } catch (error: any) {
