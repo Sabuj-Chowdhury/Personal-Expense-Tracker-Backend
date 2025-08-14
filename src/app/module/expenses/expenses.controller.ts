b6e68ee5ff7fa4b37ecd3d02c8d6d89fe3f936ad
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-08-15 05:10:02 +0600

    ADD: update single expense

--- src/app/module/expenses/expenses.controller.ts
+++ src/app/module/expenses/expenses.controller.ts
@@ -41,3 +41,33 @@ expenseRoute.get(
     }
   }
 );
+
+// ********* UPDATE single expenses
+expenseRoute.patch(
+  "/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const expenseID = req.params.id;
+      const updateExpenses = req.body;
+
+      // Only validate if amount is provided and less than and equal to 0
+      if (updateExpenses.amount !== undefined && updateExpenses.amount <= 0) {
+        throw new Error("Amount must be greater than 0!");
+      }
+
+      const expense = await Expenses.findByIdAndUpdate(
+        expenseID,
+        updateExpenses,
+        { new: true, runValidators: true }
+      );
+
+      res.status(200).json({
+        success: true,
+        message: "Expense updated successfully!",
+        data: expense,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
