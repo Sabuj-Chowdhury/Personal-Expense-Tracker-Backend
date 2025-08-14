3214c81a679052d515f9bd3cddea1ddf00bd0ea2
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-08-15 04:09:06 +0600

    ADD: expenses mongoose model

---
+++ src/app/module/expenses/expenses.model.ts
@@ -0,0 +1,34 @@
+import { model, Schema } from "mongoose";
+import { IExpenses } from "./expenses.interface";
+
+const expensesSchema = new Schema<IExpenses>(
+  {
+    title: {
+      type: String,
+      required: true,
+      minlength: [3, "Title must be at least 3 characters long"],
+    },
+    amount: {
+      type: Number,
+      required: true,
+      validate: {
+        validator: (v) => v > 0,
+        message: "Amount must be greater than 0",
+      },
+    },
+    category: {
+      type: String,
+      trim: true,
+    },
+    date: {
+      type: Date,
+      required: [true, "Date is required"],
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+export const Expenses = model<IExpenses>("Expenses", expensesSchema);
